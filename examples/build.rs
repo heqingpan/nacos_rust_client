fa3459da915d2ca564e1f9e8cbd032867d4b74b8
heqingpan <heqingpan@126.com>
2022-07-25 07:59:00 +0800

    change project example struct

---
+++ examples/build.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
+        .compile(&["proto/helloworld/helloworld.proto"], &["proto"])
+        .unwrap();
+
+    tonic_build::compile_protos("proto/echo/echo.proto").unwrap();
+}
