2082f9b4d93db61e1dfa51a9b255aaae989d69f2
heqingpan <heqingpan@126.com>
2022-07-24 22:30:23 +0800

    add init_global_system_actor

--- nacos_rust_client/src/client/nacos_client.rs
+++ nacos_rust_client/src/client/nacos_client.rs
@@ -1,5 +1,11 @@
 
-use super::HostInfo;
+use std::sync::Mutex;
+
+use crate::client::naming_client::InnerNamingListener;
+use crate::client::naming_client::UdpWorker;
+use crate::client::auth::AuthActor;
+use super::{HostInfo, config_client::ConfigInnerActor};
+use actix::{prelude::*, Context};
 
 #[derive(Debug,Clone)]
 pub struct NacosConfig {
@@ -28,5 +34,109 @@ impl NacosClient {
             config
         }
     }
+}
+
+
+pub struct ActixSystemActor {}
+
+impl ActixSystemActor {
+    pub fn new() -> Self {
+        Self{}
+    }
+}
+
+impl Actor for ActixSystemActor {
+    type Context = Context<Self>;
+
+    fn started(&mut self,ctx:&mut Self::Context){
+        log::info!("ActixSystemActor started");
+    }
+}
+
+type ActixSystemResultSender = std::sync::mpsc::Sender<ActixSystemResult>;
+
+#[derive(Message)]
+#[rtype(result="Result<ActixSystemResult,std::io::Error>")]
+enum ActixSystemCmd
+{
+    AuthActor(AuthActor,ActixSystemResultSender),
+    ConfigInnerActor(ConfigInnerActor,ActixSystemResultSender),
+    UdpWorker(UdpWorker,ActixSystemResultSender),
+    InnerNamingListener(InnerNamingListener,ActixSystemResultSender),
+}
+
+pub enum ActixSystemResult {
+    None,
+    AuthActorAddr(Addr<AuthActor>),
+    ConfigInnerActor(Addr<ConfigInnerActor>),
+    UdpWorker(Addr<UdpWorker>),
+    InnerNamingListener(Addr<InnerNamingListener>),
+}
+
+impl Handler<ActixSystemCmd> for ActixSystemActor 
+{
+    type Result = Result<ActixSystemResult,std::io::Error>;
+    fn handle(&mut self,msg:ActixSystemCmd,ctx:&mut Context<Self>) -> Self::Result {
+        match msg {
+            ActixSystemCmd::AuthActor(actor, tx) => {
+                let addr = actor.start();
+                tx.send(ActixSystemResult::AuthActorAddr(addr));
+            },
+            ActixSystemCmd::ConfigInnerActor(actor, tx) => {
+                let addr = actor.start();
+                tx.send(ActixSystemResult::ConfigInnerActor(addr));
+            },
+            ActixSystemCmd::UdpWorker(actor, tx) => {
+                let addr = actor.start();
+                tx.send(ActixSystemResult::UdpWorker(addr));
+
+            },
+            ActixSystemCmd::InnerNamingListener(actor, tx) => {
+                let addr = actor.start();
+                tx.send(ActixSystemResult::InnerNamingListener(addr));
+            },
+        }
+        Ok(ActixSystemResult::None)
+    }
+}
+
+
+lazy_static::lazy_static! {
+    static ref ACTIX_SYSTEM: Mutex<Option<Addr<ActixSystemActor>>> =  Mutex::new(None);
+}
+
+pub fn get_global_system_actor() -> Option<Addr<ActixSystemActor>> {
+    let r = ACTIX_SYSTEM.lock().unwrap();
+    r.clone()
+}
+
+pub fn set_global_system_actor(addr:Addr<ActixSystemActor>) {
+    let mut r = ACTIX_SYSTEM.lock().unwrap();
+    *r = Some(addr);
+}
+
+pub fn init_global_system_actor() -> Addr<ActixSystemActor> {
+    if let Some(r)= get_global_system_actor() {
+        return r;
+    }
+    else{
+        let addr = init_register();
+        set_global_system_actor(addr.clone());
+        return addr;
+    }
+}
+
+fn init_register() -> Addr<ActixSystemActor> {
+    let (tx,rx) = std::sync::mpsc::sync_channel(1);
+    std::thread::spawn(move || {
+        let rt = System::new();
+        let addrs = rt.block_on(async {
+            ActixSystemActor::new().start()
+        });
+        tx.send(addrs).unwrap();
+        rt.run().unwrap();
+    });
+    let addrs = rx.recv().unwrap();
+    addrs
 }
 
--- nacos_rust_client/src/lib.rs
+++ nacos_rust_client/src/lib.rs
@@ -1,1 +1,3 @@
 pub mod client;
+
+pub use client::nacos_client::init_global_system_actor;
